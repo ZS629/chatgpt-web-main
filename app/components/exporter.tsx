477ca15c94c0d2b08d04335f4ca81e551cbc42db
lizhifei <lizhifei@travelsky.com.cn>
2023-06-06 09:20:39 +0800

    1

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,7 +437,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>国内版ChatAI 工具箱</div>
             <div className={styles["sub-title"]}>
               github.com/Yidadaa/ChatGPT-Next-Web
             </div>
